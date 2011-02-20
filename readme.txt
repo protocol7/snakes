53f559685ee60afa0af5019548242349bb2028fd
Niklas Gustavsson <niklas@protocol7.com>
2011-02-20 22:16:04 +0100

    Updating documentation Adding support for async updates Cleaning up code

--- readme.txt
+++ readme.txt
@@ -1,34 +1,48 @@
-HTTP GET and PUT
-Master-less
-HTTP for sync
-Keys alphanumeric strings (limitation)
-Smallish string values only (limitation), could be json or whatever
-Stored in files with hashed keys, no buckets (limitation)
-No in-memory cache (limitation)
-No authentication (limitation)
-Logging
-No transactions (would use e-tags for optimistic locking)
-No partitioning
-Locale
+snakes is a very basic distributed key-value store. With snakes, you can run multiple nodes that are all able to act as read and write masters. Nodes will attempt to keep each other up-to-date. snakes is merely a prototype and therefore has many limitations. Below you will find a list of snakes basic features and limitations:
 
-Plan
-* Basic web server
-* Basic web client
-* Storage
-* Threads and queues
+* snakes communicates over HTTP, both with clients and among nodes. To interact as a client, you issue simple HTTP requests.
+* Keys must be alphanumeric strings
+* Values should be strings of limited size (snakes will hold and compare values in-memory). The contents of the values are opaque to snakes, it can be anything (e.g. JSON, XML)
+* snakes uses a very simple file storage. It uses one file per key. This keeps snakes simple but will not result in optimal performance as snakes will have to do a lot of seeks. SSD drives are recommended.
+* snakes does not currently support any authentication. However, snakes is meant to be placed behind a reverse proxy (such as Apache httpd or Nginx) where authentication could be performed.
+* snakes has very limited logging
+* snakes does not support any form of transactions. However, a future improvement might support optimistic locking with the use of HTTP e-tags.
+* snakes does not support partitioning
+* Nodes in a snakes cluster will sync updated among themselves, but only as long as nodes are available. During node downtime or network partitions, nodes might get out-of-date. Nodes will not actively try to sync themselves after such an incident. Instead, snakes relies on clients to update values. snakes uses a simple quorum algorithm (last update wins) to determine which value to serve a client. A future improvement might provide the client with all existing values (and update times) to choose among in order to merge a conflict.
 
-Write master:
-* snakes http://slave1:8888 http://slave2:8888
+## Starting snakes
+snakes is very simple to start. You can run as many nodes as you like. Each must have a unique IP address/port combination and a separate storage directory. To start three nodes on the same server, run:
 
-Slave:
-* snakes
-
-
-Some examples:
-Start a few nodes:
 python3.1 snakes.py http://127.0.0.1:9001 http://127.0.0.1:9002
 python3.1 snakes.py -p 9001 -d data2 http://127.0.0.1:9000 http://127.0.0.1:9002
 python3.1 snakes.py -p 9002 -d data3 http://127.0.0.1:9000 http://127.0.0.1:9001
 
-curl -v http://localhost:9000
-curl -X PUT -d val -v http://localhost:9000
+snakes requires Python 3.1.
+
+snakes will default to running on port 9000 and use a data directory named "data". To provide a different port, use the -p option. To provide a different data directory, use the -d option. You must also provide the URLs for all other nodes in the cluster. A future update of snakes might instead use mDNS to automatically locate nodes in a cluster.
+
+## Interacting as a client
+To communicate with snakes you use simple HTTP requests. 
+
+### Get a value
+To get the value of a key, issue an HTTP GET request. The examples below uses curl, but any HTTP client can be used:
+
+curl http://localhost:9002/key
+
+This request will return the value as the content of the response. If the key is not known to the cluster, a 404 response code will be returned. 
+
+You can also configure how many nodes in the snakes cluster that should be ask to provide a value for the key. You do this using the r query string parameter. By default, r=3. Here's an example:
+curl http://localhost:9002/key?r=2
+
+### Store a value
+To store (create or update) a value for a key, issue an HTTP PUT request, for example:
+
+curl -X PUT -d "some value" http://localhost:9002/key
+
+On a successful update, a 201 response code will be returned.
+
+On updates, snakes will update all nodes in a cluster. Some of these will be done synchronously before returning a response to the client. The remaining will be done on a background thread pool. By default, 3 nodes gets updated synchronously. However, you can tell snakes if you want a different number of nodes updated synchronously by using the "w" query string parameter. For example:
+
+curl -X PUT -d "some value" http://localhost:9002/key?w=4
+
+That's all there is to it. Now go and play :-)
--- snakes.py
+++ snakes.py
@@ -5,26 +5,29 @@ import os.path
 import sys
 import httplib
 import threading
+import queue
 import string
 import re
 import datetime
 import urllib.parse
 
-DFAULT_R = 3
+ASYNC_QUEUE_DEPTH = 10000
+DEFAULT_R = 3
+DEFAULT_W = 3
+NO_OF_ASYNC_THREADS = 1
 
 class SnakesHandler(http.server.BaseHTTPRequestHandler):
-            
+    """ HTTP request handler for a snakes server"""
+    
     def do_GET(self):
-        """Respond to a GET request."""
+        """Respond to a GET request for getting the value for a key """
         print("Received GET request")
         
-        r = DFAULT_R
+        # get the number of nodess to read the value from
         url = urllib.parse.urlparse(self.path)
-        if len(url.query) > 0:
-            qs = urllib.parse.parse_qs(url.query)
-            if "r" in qs:
-                r = int(qs["r"][0])
+        r = parse_int_from_qs(url.query, "r", DEFAULT_R)
         
+        # validate key
         key = url.path[1:]
         if not self.valid_key(key):
             self.send_response(403)
@@ -37,6 +40,8 @@ class SnakesHandler(http.server.BaseHTTPRequestHandler):
         
         value = None
         modified_time = datetime.datetime.min
+        
+        # do we have a value for this key locally?
         if os.path.exists(data_file):
             modified_time_stamp = os.stat(data_file).st_mtime
             modified_time = datetime.datetime.fromtimestamp(modified_time_stamp)
@@ -46,19 +51,24 @@ class SnakesHandler(http.server.BaseHTTPRequestHandler):
             with open(data_file, mode='rb') as the_file:
                 value = the_file.read(value_length)
         
-        for i in range(min(r - 1, len(self.server.slaves))):
-            slave = self.server.slaves[i]
-            (headers, content) = slave.get(key)
+        # check with r nodes to get a quorum on the value
+        for i in range(min(r - 1, len(self.server.nodes))):
+            node = self.server.nodes[i]
+            (headers, content) = node.get(key)
             
-            if not value == content:
+            if headers["status"] == 200 and not value == content:
+                
                 last_modified = datetime.datetime.strptime(headers["last-modified"], "%a, %d %b %Y %H:%M:%S %Z")
+                
+                # we use a very simple quorum, last update wins
                 if last_modified > modified_time:
                     # newer value from other node, replace
                     modified_time = last_modified
                     value = content
                     last_modified_header = headers["last-modified"]
 
-        if not value == None:
+        # respond to the client
+        if value:
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.send_header("Last-Modified", last_modified_header)
@@ -68,29 +78,25 @@ class SnakesHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
-
-        # TODO ignore?
-        #        else:
-        # print last transaction ID
-        #            self.wfile.write(str(self.server.transaction_log.transaction_seq).encode("utf-8"))
             
     def do_PUT(self):
-        """Respond to a PUT request."""
+        """Respond to a PUT request for storing a key"""
+        print("Received PUT request")
+        
         try:
             url = urllib.parse.urlparse(self.path)
             
+            # validate key
             key = url.path[1:]            
             if not self.valid_key(key):
                 self.send_response(403)
                 self.end_headers()
                 return
                 
-            print(key)
-        
             # TODO handle missing content-length
             content_length = int(self.headers.get("Content-length"))
         
-            # TODO stream
+            # set up the paths for writing
             data = self.rfile.read(content_length)
 
             tmp_file = self.server.tmp_dir + "/" + key
@@ -105,34 +111,44 @@ class SnakesHandler(http.server.BaseHTTPRequestHandler):
             
             # TODO clean up tmp if rename fails
             
-            # Ignore transaction log for now
-            # self.server.transaction_log.writeTransaction(key)
+            # push to nodes, best effort
+            w = parse_int_from_qs(url.query, "w", DEFAULT_W)
+            w = min(w, len(self.server.nodes) + 1)
             
-            # push to slaves
-            # TODO do async
-
-            qs = urllib.parse.parse_qs(url.query)
-            if not "push" in qs:
-                for slave in self.server.slaves:
-                    slave.update(key, data)
+            successfully_updated = 1
+            if w > 0:
+                for node in self.server.nodes:
+                    try:
+                        node.update(key, data, successfully_updated >= w)
+                        successfully_updated = successfully_updated + 1
+                    except Exception:
+                        pass
                 
-            self.send_response(201)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-        except IOError:
+            # respond to client
+            if successfully_updated >= w:
+                self.send_response(201)
+                self.send_header("Content-type", "text/plain")
+                self.end_headers()
+            else:
+                self.send_response(500, "Only {} nodes updated".format(successfully_updated))
+                self.end_headers()
+            
+        except Exception as err:
+            print(err)
             self.send_response(500)
             self.end_headers()
     
-    key_pattern = re.compile("^[a-zA-Z0-9]+$")
+    __key_pattern = re.compile("^[a-zA-Z0-9]+$")
     
     def valid_key(self, key):
-        return self.key_pattern.match(key)
+        return self.__key_pattern.match(key)
 
 class SnakesHttpServer(http.server.HTTPServer):
+    """ A very basic HTTP server serving requests for the snakes server """
     
-    def __init__(self, server_address, RequestHandlerClass, slaves = [], data_dir = "data"):
+    def __init__(self, server_address, RequestHandlerClass, nodes = [], data_dir = "data"):
         super( SnakesHttpServer, self ).__init__(server_address, RequestHandlerClass)
-        self.slaves = slaves
+        self.nodes = nodes
         self.data_dir = data_dir
         self.keys_dir = data_dir + "/keys"
         self.tmp_dir = data_dir + "/tmp"
@@ -141,10 +157,47 @@ class SnakesHttpServer(http.server.HTTPServer):
         if not os.path.exists(self.keys_dir): os.mkdir(self.keys_dir)
         if not os.path.exists(self.tmp_dir): os.mkdir(self.tmp_dir)
         
-        # Ignore transaction log for now
-        # self.transaction_log = TransactionLog(self.data_dir + "/transactions")
+        async_node_queue = queue.Queue(ASYNC_QUEUE_DEPTH)
+        
+        # hook up the async queue to the nodes
+        for node in nodes:
+            node.async_node_queue = async_node_queue
+        
+        for i in range(0, NO_OF_ASYNC_THREADS):
+            self.async_node_updator = AsyncNodeUpdator(async_node_queue)
+            print("Starting async updator")
+            self.async_node_updator.start()
+        
+
+class AsyncNodeUpdator(threading.Thread):
+    """ A thread for updated nodes asyncronously. Will wait for updates on a queue and update nodes as needed.  """
+    
+    http_client = httplib.Http(timeout=5)
+    stopped = False
+    
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        daemon = False
+        self.queue = queue
+    
+    def run(self):
+        while(not self.stopped):
+            try:
+                (url, data) = self.queue.get(False, 1)
+                try:
+                    print("Pushing async to node {}".format(url))
+                    self.http_client.request(url, method="PUT",body=data,headers={'content-type':'text/plain'})
+                except Exception:
+                    # reset HTTP client
+                    self.http_client = httplib.Http(timeout=5)
+            except queue.Empty:
+                pass
+
+    def stop(self):
+        self.stopped = True
 
 class SnakesNode:
+    """ Representing a specific node to which this server is connected. Handles communication with the node """
     
     http_client = httplib.Http(timeout=5)
     
@@ -156,62 +209,42 @@ class SnakesNode:
         
         self.url = node_url
         
-    def update(self, key, data):
-        print("Pushing to slave {}".format(self.url))
-
-        self.http_client.request(self.url + "/" + key + "?push=true", method="PUT", redirections=0,body=data, 
-            headers={'content-type':'text/plain'})
+    def update(self, key, data, async=False):
+        """ Update the node with the provided key and data. If async = True, the update will be requested by the AsyncNodeUpdator """
+        
+        complete_url = self.url + "/" + key + "?w=0"
+        
+        if not async:
+            print("Pushing sync to node {}".format(self.url))
+            try:
+                (headers, content) = self.http_client.request(complete_url, method="PUT", body=data, headers={'content-type':'text/plain'})
+            except Exception as e:
+                self.http_client = httplib.Http(timeout=5)
+                raise e
+        else:
+            self.async_node_queue.put((complete_url, data), False, 5)
+    
 
     def get(self, key):
+        """ Get a value based on key from the node """
+        
         print("Getting from node {}".format(self.url))
 
         (headers, content) = self.http_client.request(self.url + "/" + key + "?r=1", method="GET", redirections=0)
-        return (headers, content)
+        return (headers, content)    
 
-
-# TODO remove?
-class TransactionLog:
-    transaction_seq = 0
+def parse_int_from_qs(query, name, default):
+    """ Parse a numering value from a query string value"""
     
-    write_lock = threading.Lock()
-    
-    def __init__(self, transaction_log_file):
-        # read the last transaction ID
-        if os.path.exists(transaction_log_file):
-            transaction_log_length = os.stat(transaction_log_file).st_size
-
-            if transaction_log_length > 0:
-                print("Reading last transaction ID from transaction log")
-                with open(transaction_log_file, mode="r", encoding='utf-8') as self.transaction_log:
-                    block_size = 100
-                    seek_pos = transaction_log_length - block_size
-                    if seek_pos < 0:
-                        seek_pos = 0
-                    self.transaction_log.seek(seek_pos)
-                    data = self.transaction_log.read(block_size).split('\n')
-
-                    match = re.match("(\d+),.+", data.pop())
-                    if match:
-                        self.transaction_seq = int(match.group(1)) + 1
-                        print("New transaction sequence: {}".format(self.transaction_seq))
+    try:
+        if len(query) > 0:
+            qs = urllib.parse.parse_qs(query)
+            if name in qs:
+                return int(qs[name][0])
+    except ValueError:
+        pass
         
-            print("Appending to existing transaction log: {}".format(transaction_log_file))
-            self.transaction_log = open(transaction_log_file, mode="a", encoding='utf-8')
-        else:
-            print("Creating new transaction log: {}".format(transaction_log_file))
-            self.transaction_log = open(transaction_log_file, mode="w", encoding='utf-8')
-        
-    
-    def writeTransaction(self, key):
-        # make sure only one thread is writing at any given time
-        with self.write_lock:
-            print("writing to transaction log: {}".format(key))
-            self.transaction_log.write("\n{},{}".format(self.transaction_seq, key))
-            self.transaction_log.flush()
-            os.fsync(self.transaction_log.fileno())
-            self.transaction_seq = self.transaction_seq + 1
-    
-    
+    return default
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -230,5 +263,7 @@ if __name__ == '__main__':
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
+        
+    httpd.async_node_updator.stop()
     httpd.server_close()
     print("Server Stops - {}:{}".format("", options.port))
